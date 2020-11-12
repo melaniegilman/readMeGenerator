606be786c74c3d1166bc893eb17b673553dbebdf
Melanie Gilman <melaniegilman259@yahoo.com>
2020-11-12 13:32:32 -0500

    testing

--- index.js
+++ index.js
@@ -1,16 +1,157 @@
+const fs = require("fs");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your username on Github?',
+        validate: github => {
+            if (github) {
+                return true;
+            } else {
+                console.log("Please enter your Github Username!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your e-mail!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is name of your project?',
+        validate: projectName => {
+            if (projectName) {
+                return true;
+            } else {
+                console.log("Please enter your project's name!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("Please enter a description of your project!");
+                return false;
+            }
+        }
+     },
+     {
+        type: 'input',
+        name: 'usage',
+        message: 'Please write a short description of how to use your project.',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Please enter a description of your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project',
+        choices: ['MIT', 'GNU General', 'Apache', 'ISC', 'ODbl', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log("Please tell the user how to install dependencies!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log("Please tell the user what command should be run to run tests!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'userInfo',
+        message: 'What does the user need to know about using the repo?',
+        validate: infoInput => {
+            if (infoInput) {
+                return true;
+            } else {
+                console.log("Please tell the user what they need to know about using the repo!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing?',
+        validate: contributorInput => {
+            if (contributorInput) {
+                return true;
+            } else {
+                console.log("Please tell the user anything they need to know about contributing!");
+                return false;
+            }
+        }
+    },
 ];
 
+
 // function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = data => {
+    fs.writeFile('./newREADME.md', data, err => {
+        if (err) throw err;
+        console.log('Your README is ready to view!')
+    })
 }
 
 // function to initialize program
-function init() {
-
+const init = () => {
+    console.log("Welcome! Follow the prompts to create your README")
+    return inquirer.prompt(questions);
 }
 
 // function call to initialize program
-init();
+init() 
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then (finalTemplate => {
+        return writeToFile(finalTemplate);
+    })
+    .catch (err => {
+        console.log(err);
+    });
---
+++ newREADME.md
@@ -0,0 +1,27 @@
+
+    # Wilbur
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    ## Description
+    An application that helps users keep track of caring for their pets.  Check-ins for vet care, feedings, water intake, and more are a few of the things this app includes to help a user track their pet care.
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributors)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    ## Installation
+    To install the necessary dependencies, run the following command in your command line: 
+    npm install
+    ## Usage
+    Log into application, fill out desired information, allow push notifications, and simply let the app keep track for you!
+    ## License 
+    This project is covered by MIT.
+    ## Contributing
+    This repo is public and open to contributors
+    ## Tests
+    To run tests, run the following command: 
+    npm run tests
+    ## Questions
+    If you have questions about the project or run into issues, contact me at melaniegilman259@yahoo.com. You can access more of my work at [melaniegilman](https://github.com/melaniegilman)
+  
